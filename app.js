8490b7c39a95a926d6fbc2ec1690e7292807ff4d
Ali Beroro <a.beroro@digieye.io>
2022-06-11 14:10:12 +0100

    ssl

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+//ssl
+const fs = require('fs');
+const https = require('https');
+
 const server = http.createServer(app);
 const cors=require('cors')
 // Connect to pstgres
 const sequelize=require('./config/sequelize');
 const routes = require('./routes')
-    
+
+
+
 app.use(cors());
 app.use(express.json())
 app.get('/', (req, res) => {
@@ -17,6 +23,18 @@ app.get('/', (req, res) => {
 
 app.use('/api',routes)
 
-server.listen(5000, () => {
+
+//SSL
+var privateKey  = fs.readFileSync('api.baseball-gifts.com/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('api.baseball-gifts.com/fullchain.pem', 'utf8');
+//var ca = [ fs.readFileSync('ca_bundle.crt', 'utf8') ];
+var credentials = {key: privateKey, cert: certificate};
+
+var httpsServer = https.createServer(credentials, app);
+
+
+httpsServer.listen(5000,()=>{console.log('listening on *:4000');});
+
+/* server.listen(5000, () => {
   console.log('listening on *:5000');
-});
+}); */
